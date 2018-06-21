76ce34a8153f102f8792ec929463299b7a848516
Julio César <zzantares@gmail.com>
2018-06-21 00:13:24 -0500

    Add node pseudo code
    Supposedly this is how the node part should work, but it's not been tested.

--- multipylib/node.py
+++ multipylib/node.py
@@ -1,3 +1,48 @@
+import multiprocessing
+import queue
+from multiprocessing.managers import SyncManager
+
+
+def worker(task_queue, result_queue):
+    # Run until task_queue is empty
+    while True:
+        try:
+            task, kwargs = task_queue.get_nowait()
+            task_result = task(
+                **kwargs)  # Call the function with its parameters
+            result_queue.put(task_result)
+        except queue.Empty:
+            return
+
+
+def connect_server(host, port, authkey):
+    class ServerManager(SyncManager):
+        pass
+
+    ServerManager.register('get_task_queue')
+    ServerManager.register('get_result_queue')
+
+    manager = ServerManager(address=(host, port), authkey=authkey)
+    manager.connect()
+
+    print('Client connected to {}:{}'.format(host, port))
+
+    return manager
+
+
 def start(args):
-    print('This is in the multipy/node.py file')
-    print(args)
+    manager = connect_server(args.host, args.port, args.authkey)
+    task_queue = manager.get_task_queue()
+    result_queue = manager.get_result_queue()
+
+    # Start to run a worker per process
+    procs = []
+    for _ in range(args.procs):
+        p = multiprocessing.Process(
+            target=worker, args=(task_queue, result_queue))
+        procs.append(p)
+        p.start()
+
+    # Wait for all workers to finish before terminating main process
+    for p in procs:
+        p.join()
